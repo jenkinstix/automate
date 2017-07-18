9bac1d28e9a2b4e039c38c42f80b2e42d42cdfd2
adam h <jenkinstix@mail.com>
2017-07-18 14:37:36 -0700

    added second test

---
+++ tests/e2e/test2.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'Body Color' : function (browser) {
+    browser
+      .url('http://localhost:3000/index.html')
+      .waitForElementVisible('body', 1000)
+      .assert.cssProperty('body', 'background-color', 'green')
+      .end();
+  }
+};
